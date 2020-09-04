034c8a0df576848a161e30d21ff73760b7c9b310
adrianroman <adi.roman94@gmail.com>
2020-09-04 13:53:04 +0300

    Preview Image scales correctly

---
+++ Assets/Scripts/ScaleImage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScaleImage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {   
+        float cam = Camera.main.aspect;
+
+        if (cam < 1.5f)
+        {
+            this.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+        }
+
+        else if (cam > 2f)
+        {
+            this.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        }
+    }
+}
