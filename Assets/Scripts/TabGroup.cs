4b607621c5a59bb9a5127ae6313e612f07fe172a
adrianroman <adi.roman94@gmail.com>
2020-10-09 18:42:45 +0300

    Custom Tab System

---
+++ Assets/Scripts/TabGroup.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<TabButton> tabButtons;
+    public Color newColor;
+    public TabButton selectedTab;
+    public List<GameObject> objectsToSwap;
+
+    public void Subscribe(TabButton button)
+    {
+        if(tabButtons == null)
+        {
+            tabButtons = new List<TabButton>();
+        }
+
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButton button)
+    {
+        ResetTabs();
+        if (selectedTab == null || button != selectedTab)
+        {
+            ColorBlock cb = button.GetComponent<Button>().colors;
+            cb.highlightedColor = newColor;
+            button.GetComponent<Button>().colors = cb;
+        }
+    }
+
+    public void OnTabExit(TabButton button)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(TabButton button)
+    {
+        selectedTab = button;
+        ResetTabs();
+        ColorBlock cb = button.GetComponent<Button>().colors;
+        cb.selectedColor = newColor;
+        button.GetComponent<Button>().colors = cb;
+        int index = button.transform.GetSiblingIndex();
+        for (int i = 0; i < objectsToSwap.Count; i++)
+        {
+            if (i == index)
+            {
+                objectsToSwap[i].SetActive(true);
+            }
+            else
+            {
+                objectsToSwap[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ResetTabs()
+    {
+        foreach(TabButton button in tabButtons)
+        {
+            if(selectedTab != null && selectedTab) { continue; }
+            ColorBlock cb = button.GetComponent<Button>().colors;
+            cb.normalColor = newColor;
+            button.GetComponent<Button>().colors = cb;
+        }
+    }
+}
