a961104164e229850b8fb9b3407db71fb3c713fd
adrianroman <adi.roman94@gmail.com>
2020-11-10 19:32:57 +0200

    *

---
+++ Assets/1Scripts/ProgressBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ProgressBar : MonoBehaviour
+{
+    public int current;
+    public int maximum;
+
+    public Image fill;
+    void Update()
+    {
+        GetCurrentFillAmount();
+    }
+
+    // Update is called once per frame
+    void GetCurrentFillAmount()
+    {
+        float fillAmount = (float)current / (float)maximum;
+        fill.fillAmount = fillAmount;
+    }
+}
