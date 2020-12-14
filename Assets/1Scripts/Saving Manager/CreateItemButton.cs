45d7cc3e3f5ca6dd78954c91e201562608760107
Anca <anca.rosu95@gmail.com>
2020-12-15 00:12:05 +0200

    Fix Creeaza - Backgrounds and read file

--- Assets/1Scripts/Saving Manager/CreateItemButton.cs
+++ Assets/1Scripts/Saving Manager/CreateItemButton.cs
@@ -11,14 +11,13 @@ public class CreateItemButton : MonoBehaviour
     public static List<string> SavedItems = new List<string>();
     public string ItemNameAndType;
     public bool activeByDefault = false;
-    public string SubmenuType = "Submenu";
+    public string ElementType;
     // Start is called before the first frame update
 
     private void Start()
     {
         SetupSavedItems();
-
-        SubmenuType = transform.parent.parent.parent.name == "1ScrollFundal" ? "SmallSubmenu" : "Submenu";
+        SetupType();
 
         string elementName = gameObject.GetComponent<Image>().sprite.name.Replace("_thumbnail", "");
         string foundItem = SavedItems.Find(s => s.Split(',')[0].Equals(elementName)); // split from the free roam list
@@ -37,7 +36,7 @@ public class CreateItemButton : MonoBehaviour
     {
         if (String.IsNullOrWhiteSpace(foundItem))
         {
-            ItemNameAndType = gameObject.GetComponent<Image>().sprite.name.Replace("_thumbnail", "") + "," + SubmenuType;
+            ItemNameAndType = gameObject.GetComponent<Image>().sprite.name.Replace("_thumbnail", "") + "," + ElementType;
         }
         else ItemNameAndType = foundItem;
     }
@@ -45,7 +44,16 @@ public class CreateItemButton : MonoBehaviour
     private void SetupSavedItems()
     {
         if (SavedItems.Count > 0) return;
-        SavedItems = File.ReadAllLines(GetFilePath("creeaza.json")).ToList<string>();
+        if (File.Exists(GetFilePath("creeaza.json")))
+        {
+            SavedItems = File.ReadAllLines(GetFilePath("creeaza.json")).ToList<string>();
+        }
+    }
+
+    private void SetupType() 
+    {
+        if (!String.IsNullOrWhiteSpace(ElementType)) return;
+        ElementType = transform.parent.parent.parent.name == "1ScrollFundal" ? "SmallSubmenu" : "Submenu";
     }
 
     private string GetFilePath(string saveFile)
